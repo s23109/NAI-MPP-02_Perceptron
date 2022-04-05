c202224c9aae2888e5f2f20060efbfe64597aa62
s23109 <s23109@pjwstk.edu.pl>
2022-04-05 13:07:51 +0200

    Funkcja zgadywania, przyjęte założenia wyjść

--- src/com/perceptron/Main.java
+++ src/com/perceptron/Main.java
@@ -12,10 +12,18 @@ public class Main {
 
         //File name is hard-coded
         //"Dane\\perceptron.data.txt"
+
+        /*
+        Wariant versicolor = 0
+        Wariant virginica = 1
+         */
+        final int learn_loop_limit = 250;
         final double stala_uczenia = 0.01;
         final double akceptowalny_procent_bladow = 0.02 ;
         List<Element> zbior_treningowy = File_operations.list_from_file("Dane\\perceptron.data.txt");
         List<Element> zbior_testowy = File_operations.list_from_file("Dane\\perceptron.test.data.txt");
+
+        Perceptron perceptron = new Perceptron(zbior_treningowy.get(0).coordinates.size(),stala_uczenia,akceptowalny_procent_bladow);
 
 
 
--- src/com/perceptron/Perceptron.java
+++ src/com/perceptron/Perceptron.java
@@ -4,21 +4,26 @@ import java.util.List;
 
 public class Perceptron {
 
-    //perceptron do rozrózniania versicolor od drugiego
+    /*
+        Wariant versicolor = 0
+        Wariant virginica = 1
+    */
 
-    final double stala_uczenia = 0.01;
-    final double akceptowalny_procent_bladow = 0.02 ;
+    final double stala_uczenia ;
+    final double akceptowalny_procent_bladow ;
     double prog_odchylenia = 3 ;
     private List<Double> wagi ;
     private int number_of_wagi ;
 
-    public Perceptron(int number_of_wagi) {
+    public Perceptron(int number_of_wagi , double stala_uczenia , double procent_bledow) {
         this.number_of_wagi = number_of_wagi;
         System.out.print("Wagi Perceptronu: ");
         for (int i = 0; i < number_of_wagi; i++) {
             wagi.add((Math.random()-0.15)*2.0*(Math.random()+0.15>0.5?-1:1));
             System.out.print(wagi.get(i) + " ");
         }
+        this.stala_uczenia=stala_uczenia;
+        this.akceptowalny_procent_bladow=procent_bledow;
         System.out.println("------------\nPerceptron created");
     }
 
@@ -45,6 +50,27 @@ public class Perceptron {
             this.wagi = nowa_waga;
             this.prog_odchylenia= prog_odchylenia-stala_uczenia*(prawdziwy-szacowany);
 
+    }
+
+    private int guess (List<Double> tested){
+        /*
+        Wariant versicolor = 0
+        Wariant virginica = 1
+         */
+        Double net = 0.0;
+
+        for (int i = 0; i < tested.size(); i++) {
+            net+=tested.get(i)*wagi.get(i);
+        }
+
+        net+=prog_odchylenia;
+
+        if (net >= 0) {
+            //virginica
+            return 1;
+        }
+        //versicolor
+        return 0;
     }
 
 
