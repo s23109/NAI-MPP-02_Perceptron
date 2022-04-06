3dbd7f7d828a6c1560990758f74a590be53554cf
D K <s23109@pjwstk.edu.pl>
2022-04-06 19:22:35 +0200

    Perceptron dziala , proste zmiany czytelności kodu perceptronu, BRAK USER INPUT-u, BRAK TEST-Danych

--- src/com/perceptron/Main.java
+++ src/com/perceptron/Main.java
@@ -1,4 +1,5 @@
 package com.perceptron;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -7,7 +8,6 @@ public class Main {
 
         //File name is hard-coded
         //"Dane\\perceptron.data.txt"
-
         /*
         Wariant versicolor = 0
         Wariant virginica = 1
@@ -16,10 +16,14 @@ public class Main {
         boolean loop_bool = true;
         int loop_iterations=0;
 
-        final double stala_uczenia = 0.05;
-        final double akceptowalny_procent_bladow = 0.02 ;
+        final double stala_uczenia = 0.0001;
+        final double akceptowalny_procent_bladow = 0.025 ;
         List<Element> zbior_treningowy = File_operations.list_from_file("Dane\\perceptron.data.txt");
         List<Element> zbior_testowy = File_operations.list_from_file("Dane\\perceptron.test.data.txt");
+
+        List<String> pomo_typy = new ArrayList<>();
+        pomo_typy.add("versicolor");
+        pomo_typy.add("virginica");
 
         Perceptron perceptron = new Perceptron(zbior_treningowy.get(0).coordinates.size(),stala_uczenia,akceptowalny_procent_bladow);
 
@@ -34,41 +38,47 @@ public class Main {
             System.out.print(loop_iterations + ". ");
             perceptron.wypisz_wagi();
 
-            for (Element e: zbior_treningowy
-                 ) {
+            for (Element e: zbior_treningowy) {
                 ilosc_testowanych+=1.0;
-                int guessed = perceptron.guess(e.coordinates);
+                //Pętla nauki + zgadywania
+                //ilosc_poprawnie_klasyfikowanych+=1.0;
+                int guess = perceptron.guess(e);
                 /*
                  Wariant versicolor = 0
-                 Wariant virginica = 1
+                Wariant virginica = 1
                 */
 
-                if ( (e.name_of_object.equals("Iris-versicolor") && guessed == 0) || e.name_of_object.equals("Iris-virginica") && guessed == 1 ) {
-                    //zgadnięto element
+                if (e.name_of_object.contains(pomo_typy.get(guess))){
+                    //jeśli trafiony, to git
                     ilosc_poprawnie_klasyfikowanych+=1.0;
                 }
                 else {
-                    perceptron.learn(e.coordinates, guessed , (e.name_of_object.equals("Iris-versicolor") ? 0 : 1 ));
+                    perceptron.learn(e);
                 }
+
+
+
             }
+
 
             //check if learned enough or limit
             if (loop_iterations == learn_loop_limit )  {
                 loop_bool=false;
-                System.out.println("Osiągnięto limit petli nauki. Margines bledu wymagany:"
+                System.out.println("-----------------------\nOsiągnięto limit petli nauki. Margines bledu wymagany:"
                         + akceptowalny_procent_bladow
-                        + " a osiągnięto  " + (double)(1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych))
+                        + " a osiągnięto  " + (1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych))
                 );
 
             }
-
-            if ( (double)(1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych)) <= akceptowalny_procent_bladow) {
+            else {
+                System.out.println("Aktualna dokładność: " + (1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych)) + "\n-----------------------");
+            }
+            if ( (1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych)) <= akceptowalny_procent_bladow) {
                 loop_bool=false;
-                System.out.println("Osiągnięto wymagany margines błędu równy " + (double)(1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych))
+                System.out.println("Osiągnięto wymagany margines błędu równy " + (1.0 - (ilosc_poprawnie_klasyfikowanych/ilosc_testowanych))
                 + " który jest mniejszy/równy niż " + akceptowalny_procent_bladow
                 );
             }
-
             loop_iterations+=1;
         }
 
--- src/com/perceptron/Perceptron.java
+++ src/com/perceptron/Perceptron.java
@@ -10,71 +10,68 @@ public class Perceptron {
         Wariant virginica = 1
     */
 
-    final double stala_uczenia ;
-    final double akceptowalny_procent_bladow ;
-    double prog_odchylenia = 3 ;
+    final double stala_uczenia;
+    final double akceptowalny_procent_bladow;
+    private double Teta;
     private List<Double> wagi = new ArrayList<>();
-    private int number_of_wagi ;
+    private int number_of_wagi;
 
-    public Perceptron(int number_of_wagi , double stala_uczenia , double procent_bledow) {
+    public Perceptron(int number_of_wagi, double stala_uczenia, double procent_bledow) {
         this.number_of_wagi = number_of_wagi;
         System.out.print("Wagi Perceptronu: ");
         for (int i = 0; i < number_of_wagi; i++) {
-            wagi.add((Math.random()-0.15)*2.0*(Math.random()+0.15>0.5?-1:1));
+            wagi.add((Math.random() - 0.15) * 2.0 * (Math.random() + 0.15 > 0.5 ? -1 : 1));
             System.out.print(wagi.get(i) + " ");
         }
-        this.stala_uczenia=stala_uczenia;
-        this.akceptowalny_procent_bladow=procent_bledow;
+        this.stala_uczenia = stala_uczenia;
+        this.akceptowalny_procent_bladow = procent_bledow;
+        this.Teta = Math.random() * 2;
         System.out.println("\nPerceptron created\n------------");
     }
 
-    public void wypisz_wagi (){
+    public void wypisz_wagi() {
         System.out.print("Wagi : ");
-        for (Double e: wagi
-             ) {
+        for (Double e : wagi
+        ) {
             System.out.print(e + " ");
         }
 
-        System.out.print("\nProg odchylenia: "+ prog_odchylenia + "\n");
+        System.out.print("\nProg odchylenia: " + Teta + "\n");
     }
 
-    public void learn (List<Double> tested , int szacowany , int prawdziwy ) {
+    public void learn(Element e) {
+        int d = (e.name_of_object.contains("versicolor")?0:1);  //oczekiwana
+        int y = guess(e);       //aktualnie szacowana
 
-            List<Double> nowa_waga = new ArrayList<>();
+        for (int i = 0; i < wagi.size(); i++) {
+            wagi.set(i,(wagi.get(i)+(d-y)*stala_uczenia*e.coordinates.get(i)));
+        }
 
-            for (int i = 0; i < tested.size(); i++) {
 
-                nowa_waga.add(wagi.get(i)+(stala_uczenia*(prawdziwy-szacowany))*tested.get(i));
-
-            }
-
-            this.wagi = nowa_waga;
-            this.prog_odchylenia= prog_odchylenia-stala_uczenia*(prawdziwy-szacowany);
-
+        this.Teta= (Teta-(d-y)*stala_uczenia);
     }
 
-    public int guess (List<Double> tested){
+    private double obliczNet(Element element) {
+        double net = 0;
+        for (int i = 0; i < wagi.size(); i++) {
+            net += wagi.get(i) * element.getCoordinates().get(i);
+        }
+        return (net-this.Teta);
+    }
+
+    private boolean funkcja_aktywacji (Double net){
+        return (net >= 0);
+    }
+
+    public int guess(Element element) {
         /*
         Wariant versicolor = 0
         Wariant virginica = 1
          */
-        Double net = 0.0;
-
-        for (int i = 0; i < tested.size(); i++) {
-            net+=tested.get(i)*wagi.get(i);
-        }
-
-        net+=prog_odchylenia;
-
-        if (net >= 0) {
-            //virginica
+        if (funkcja_aktywacji(obliczNet(element))) {
             return 1;
         }
-        //versicolor
         return 0;
     }
-
-
-
 
 }
